d2ab0eaf08bec1e1550516bb8a223e597550c098
Thomas <Thomas.facquet@yousign.com>
2022-12-15 15:20:29 +0100

    add pom 2

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 ---
 
-
 ### Langage, framework :
 
 Tests scenarios are written in JAVASCRIPT with the help of CYPRESS framework.
@@ -10,9 +9,10 @@ A Gherkin layer is used with CUCUMBER PREPROCESSOR
 
 ### Pre-requisities / dependencies :
 
-| Dependency                    | Version |
-| ----------------------------- | ------- |
+| Dependency                    | Version  |
+| ----------------------------- | -------- |
 | cypress                       | `12.1.0` |
+| cypress-cucumber-preprocessor | `15.0.0` |
 
 ---
 
@@ -23,3 +23,37 @@ git clone https://github.com/thomasfacquet/follow_test.git
 cd follow_test
 npm install
 ```
+
+Then
+
+- `npm run cy:open` : to launch Cypress
+- `npm run cy:run`: to launch headless test suite
+- `npm run cy:run:dashboard`: to launch headless test suite with execution results on Cypress Dashboard
+
+---
+
+### INTEGRATIONS / ACTIONS
+
+- You can find a DASHBOARD of tests runs here : https://cloud.cypress.io/projects/cuszxx/runs
+
+---
+
+### Files :
+
+- `.feature` files : Gherkin steps
+- `.js`files : Step definitions of gherkin steps
+- `pages/` folder : Page Object Model helpers for each page or component (locators, functions)
+
+---
+
+### In scope :
+
+Advanced search non-regression test for a trainer :
+
+- Access to the website
+- Access to the advanced research
+- Checking the presence of search filters
+- Advanced research by type
+- Advanced research by weakness
+- Advanced research by type and weakness
+- Advanced research with no criteria
--- cypress/support/pages/ResearchPage.js
+++ cypress/support/pages/ResearchPage.js
@@ -1,4 +1,12 @@
 export class ResearchPage {
+  visitWebsite() {
+    cy.intercept("https://www.pokemon.com/pcom/api/gus/pcom/fr/").as(
+      "displayOk"
+    );
+    cy.visit("/");
+    cy.wait("@displayOk");
+  }
+
   getAlertDialog() {
     cy.get("[role=alertdialog]").should("be.visible");
   }
@@ -11,5 +19,77 @@ export class ResearchPage {
     return cy
       .get(".filter-toggle-span")
       .contains("Afficher la recherche avancée");
+  }
+
+  getFilterList() {
+    return cy.get(".pokedex-filter-tw-list").should("be.visible");
+  }
+
+  getResearchButton() {
+    return cy
+      .get("[id=search]")
+      .should("be.visible")
+      .should("contain.text", "Rechercher");
+  }
+
+  getResetButton() {
+    cy.get("[id=reset]")
+      .should("be.visible")
+      .should("contain.text", "Réinitialiser");
+  }
+
+  getMinRange() {
+    return cy
+      .get("#minRangeBox")
+      .should("be.visible")
+      .should("have.value", "1");
+  }
+
+  getMaxRange() {
+    return cy
+      .get("#maxRangeBox")
+      .should("be.visible")
+      .should("have.value", "905");
+  }
+
+  getFilterType(rank) {
+    return cy
+      .get(".pokedex-filter-tw-list")
+      .find(".type-filter")
+      .eq(rank)
+      .should("be.visible");
+  }
+
+  checkFilterResult(count) {
+    cy.get(".results")
+      .find(".animating")
+      .should("be.visible")
+      .should("have.have.length", count);
+  }
+
+  checkResult(name) {
+    return cy
+      .get(".pokedex-results")
+      .find(".animating")
+      .eq(0)
+      .and("include.text", name);
+  }
+
+  checkResultLevel(rank) {
+    return cy.get(".pokemon-info").find(".id").eq(0).and("include.text", rank);
+  }
+
+  getFilterWeakness(rank) {
+    return cy
+      .get(".pokedex-filter-tw-list")
+      .find(".weakness-filter")
+      .eq(rank)
+      .should("be.visible");
+  }
+
+  getNoResults() {
+    cy.get(".alert")
+      .should("be.visible")
+      .should("include.text", "Aucun Pokémon n'a été trouvé !");
   }
 }
